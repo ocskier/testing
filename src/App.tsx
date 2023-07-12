5190445aeaa0701a23f30a23051a9333605c51c0
Jon Jackson <ocskier@gmail.com>
2023-07-11 23:54:26 -0400

    Adds tesst

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import { ThemeProvider } from "@mui/material";
-import React from "react";
+import { ThemeProvider } from '@mui/material';
+import React from 'react';
 
-import theme from "./theme";
-import Home from "./components/Home";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import "./assets/style/root.css";
-import Portfolio from "./components/Portfolio";
-import AboutMe from "./components/AboutMe";
-import ContactForm from "./components/ContactForm";
-import Menu from "./components/Menu";
+import theme from './theme';
+import Home from './components/Home';
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+import './assets/style/root.css';
+import Portfolio from './components/Portfolio';
+import AboutMe from './components/AboutMe';
+import ContactForm from './components/ContactForm';
+import Menu from './components/Menu';
 
 function App() {
   return (
@@ -16,11 +21,11 @@ function App() {
       <div>
         <Router>
           <Routes>
-            <Route path="/tobyjportfolio/aboutme" element={<AboutMe />} />
-            <Route path="/tobyjportfolio/portfolio" element={<Portfolio />} />
-            <Route path="/tobyjportfolio/contact" element={<ContactForm />} />
-            <Route path="/tobyjportfolio/menu" element={<Menu />} />
-            <Route path="/tobyjportfolio/" element={<Home />} />
+            <Route index element={<Home />} />
+            <Route path="aboutme" element={<AboutMe />} />
+            <Route path="portfolio" element={<Portfolio />} />
+            <Route path="contact" element={<ContactForm />} />
+            <Route path="menu" element={<Menu />} />
           </Routes>
         </Router>
       </div>
