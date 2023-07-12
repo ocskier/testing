5661fd3196bca9a9f9c206afa9a83066b32f64fe
Jon Jackson <ocskier@gmail.com>
2023-07-12 00:33:31 -0400

    Adds tesst

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,54 +1,54 @@
-import React from "react";
-import { Box, Grid, Button, useMediaQuery } from "@mui/material";
-import tabletBG from "../assets/largeSVG.svg";
+import React from 'react';
+import { Box, Grid, Button, useMediaQuery } from '@mui/material';
+import tabletBG from '../assets/largeSVG.svg';
+import { Link } from 'react-router-dom';
 
 const colors = [
-  "#FFE597",
-  "#919FF8",
-  "#FE7075",
-  "#FE555D",
-  "#5B71EA",
-  "#FFCF51"
+  '#FFE597',
+  '#919FF8',
+  '#FE7075',
+  '#FE555D',
+  '#5B71EA',
+  '#FFCF51',
 ];
 
 const Menu = () => {
-  const smallScreen = useMediaQuery("(max-width: 600px)");
+  const smallScreen = useMediaQuery('(max-width: 600px)');
 
   let boxHeight;
   let boxWidth;
   let fSize;
   let buttonw;
   if (smallScreen) {
-    boxHeight = "30vh";
-    boxWidth = "45vw";
-    fSize = "15px";
-    buttonw = "130px";
+    boxHeight = '30vh';
+    boxWidth = '45vw';
+    fSize = '15px';
+    buttonw = '130px';
   } else {
-    boxHeight = "40vh";
-    boxWidth = "40vw";
-    fSize = "20px";
-    buttonw = "250px";
+    boxHeight = '40vh';
+    boxWidth = '40vw';
+    fSize = '20px';
+    buttonw = '250px';
   }
 
   const boxStyle = {
     height: boxHeight,
-    border: "1px solid white",
-    borderRadius: "50px",
+    border: '1px solid white',
+    borderRadius: '50px',
     width: boxWidth,
-    margin: "20px",
-    backdropFilter: "blur(55px) brightness(80%)",
+    margin: '20px',
+    backdropFilter: 'blur(55px) brightness(80%)',
     background:
-      "linear-gradient(45deg, rgba(94, 64, 64, 0.3) 0%, rgba(64, 64, 64, 0.08) 70%)",
-    
+      'linear-gradient(45deg, rgba(94, 64, 64, 0.3) 0%, rgba(64, 64, 64, 0.08) 70%)',
   };
 
   const buttonStyle = {
-    backgroundColor: "white",
-    color: "black",
+    backgroundColor: 'white',
+    color: 'black',
     fontSize: fSize,
-    padding: "10px 20px",
+    padding: '10px 20px',
     width: buttonw,
-    height: "25%",
+    height: '25%',
   };
 
   return (
@@ -59,54 +59,78 @@ const Menu = () => {
       height="100vh"
       sx={{
         backgroundImage: `url(${tabletBG})`,
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        padding: "0 20px"
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        padding: '0 20px',
       }}
     >
       <Grid container spacing={4}>
-        <Grid item xs={6} display="flex" flexDirection="column" alignItems="center">
+        <Grid
+          item
+          xs={6}
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+        >
           <Box bgcolor={colors[4]} sx={boxStyle}>
             <Box
               sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height: "100%"
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100%',
               }}
             >
-              <Button href="./aboutme" variant="contained" color="primary" style={buttonStyle}>
-                About Me
-              </Button>
+              <Link to="/aboutme">
+                <Button variant="contained" color="primary" style={buttonStyle}>
+                  About Me
+                </Button>
+              </Link>
             </Box>
           </Box>
           <Box bgcolor={colors[4]} sx={boxStyle}>
             <Box
               sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height: "100%"
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100%',
               }}
             >
-              <Button href="./resume" variant="contained" color="primary" style={buttonStyle}>
+              <Button
+                href="./resume"
+                variant="contained"
+                color="primary"
+                style={buttonStyle}
+              >
                 Resume
               </Button>
             </Box>
           </Box>
         </Grid>
-        <Grid item xs={6} display="flex" flexDirection="column" alignItems="center">
+        <Grid
+          item
+          xs={6}
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+        >
           <Box bgcolor={colors[4]} sx={boxStyle}>
             <Box
               sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height: "100%"
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100%',
               }}
             >
-              <Button href="./portfolio" variant="contained" color="primary" style={buttonStyle}>
+              <Button
+                href="./portfolio"
+                variant="contained"
+                color="primary"
+                style={buttonStyle}
+              >
                 Portfolio
               </Button>
             </Box>
@@ -114,13 +138,18 @@ const Menu = () => {
           <Box bgcolor={colors[4]} sx={boxStyle}>
             <Box
               sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height: "100%"
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100%',
               }}
             >
-              <Button href="./contact" variant="contained" color="primary" style={buttonStyle}>
+              <Button
+                href="./contact"
+                variant="contained"
+                color="primary"
+                style={buttonStyle}
+              >
                 Contact Me
               </Button>
             </Box>
